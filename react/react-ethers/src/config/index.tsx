5e79da60dac9a4a6e156d99915ed9876dd83c639
Greg Scullard <greg@swirldslabs.com>
2025-04-25 12:24:34 +0200

    Use hedera config

--- react/react-ethers/src/config/index.tsx
+++ react/react-ethers/src/config/index.tsx
@@ -1,6 +1,7 @@
-import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks'
+// import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+import {hederaTestnet} from "./hedera.ts";
 
 // Get projectId from https://cloud.reown.com
 export const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
@@ -18,7 +19,8 @@ export const metadata = {
 }
 
 // for custom networks visit -> https://docs.reown.com/appkit/react/core/custom-networks
-export const networks = [mainnet, arbitrum, sepolia] as [AppKitNetwork, ...AppKitNetwork[]]
+// export const networks = [mainnet, arbitrum, sepolia] as [AppKitNetwork, ...AppKitNetwork[]]
+export const networks = [hederaTestnet] as [AppKitNetwork, ...AppKitNetwork[]]
 
 // Set up Solana Adapter
 export const ethersAdapter = new EthersAdapter();
