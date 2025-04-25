7ef7e1f65e9e8a973265222a9a97c3ee71f66b73
Greg Scullard <greg@swirldslabs.com>
2025-04-25 12:24:20 +0200

    Add hedera config

---
+++ react/react-ethers/src/config/hedera.ts
@@ -0,0 +1,26 @@
+import { defineChain } from '@reown/appkit/networks';
+
+// Define the custom network
+export const hederaTestnet = defineChain({
+    id: 296,
+    caipNetworkId: 'eip155:296',
+    chainNamespace: 'eip155',
+    name: 'Hedera',
+    nativeCurrency: {
+        decimals: 8,
+        name: 'Hbar',
+        symbol: 'HBAR',
+    },
+    rpcUrls: {
+        default: {
+            http: ['https://testnet.hashio.io/api'],
+            webSocket: ['https://testnet.hashio.io/api:8546'],
+        },
+    },
+    blockExplorers: {
+        default: { name: 'Explorer', url: 'https://hashscan.io' },
+    },
+    contracts: {
+        // Add the contracts here
+    }
+})
